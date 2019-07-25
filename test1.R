4fac6c99bd82039971012ec6ad2c68503ca255ad
unknown <hklinke@LMGKKHI-NB-1907.ads.mwn.de>
2019-07-25 14:46:41 +0200

    Guthub app, user name changed

--- test1.R
+++ test1.R
@@ -1,2 +1,4 @@
 This is surely a test to sync and push to github with a second invited user.
 This appears as "unknown" user
+changed .gitconfig name - no result, needs to be ack
+change in github app.
