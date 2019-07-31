a127081fc2fc945b340896642254564d1bd502af
Fernando <fdomunozmartin@gmail.com>
2019-07-31 14:26:22 +0200

    Code to download the images

--- load_images.R
+++ load_images.R
@@ -1,2 +1,15 @@
 #load_images.R
 message("load_images.R")
+
+
+max=length(carmen_data_utf.8$work_image_URL.href)
+
+for (i in 1:max) {
+  filename <- paste("images/", carmen_data_utf.8$work_ID[i], ".jpg", collapse = "", sep = "")
+  # message(i, " ", filename)
+  download.file(url = carmen_data_utf.8$work_image_URL.href[i], 
+                destfile = filename,  
+                mode = "wb")
+  print(i/max*100)
+}
+
