904a3f78244398711dd249fc13eddf9325987c15
gqz <gqz@gqzdeMacBook-Pro.local>
2019-07-31 13:16:38 +0200

    I have loaded the data

--- load_data.R
+++ load_data.R
@@ -1,3 +1,6 @@
 #load_data
 
 message("load_data")
+carmen_data_utf.8 <- read.csv("http://harald-klinke.de/projects/content/2-dahss19/2-our-data/carmen_data_utf-8.csv",
+                              encoding="UTF-8", comment.char="#", stringsAsFactors = FALSE)
+
