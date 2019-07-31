3f16bca2eaec9931ea1e07f3b0628481ef7ca9e7
Harald Klinke <h.klinke@lmu.de>
2019-07-31 12:41:50 +0200

    Added tasks

--- main.R
+++ main.R
@@ -2,5 +2,18 @@
 message("Running main")
 
 
+#Load data (from a URL)
+
+#URL images download them
+
+#get average color (optional) total, rgb
+
+#get dimensions of images
+
+#get creation date as numeric
+
+
+
+
 
 #source("load_data.R")
