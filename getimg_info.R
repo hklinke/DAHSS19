f3f5ec0808083bf8abe7bb6e6ffa844e21d51384
Harald Klinke <h.klinke@lmu.de>
2019-07-31 16:43:19 +0200

    Created new module files

---
+++ getimg_info.R
@@ -0,0 +1,1 @@
+#getimg_info.R
--- main.R
+++ main.R
@@ -16,8 +16,24 @@ source("load_images.R")
 #get creation date as numeric Victor
 source("create_date.R")
 
+#get information from images (average and dimensions) Fernando
+source("getimg_info.R")
+
+#sort data by creation date 	Qiuzi
+source("sort_by.R")
+
+#Create keyword/hashtags Fernando
+source("create_keyword.R")
+
+#Create summary (optional) from description	Harald
+source("create_summary.R")
+
+#Create tweet string with max. characters 	Victor
+source("create_string.R")
+
+
+
 #get average color (optional) total, rgb
 #source("get_average.R")
-
 #get dimensions of images
 #source("get_dims.R")
