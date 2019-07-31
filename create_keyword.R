531e162f69123dbcf0ff9e5eaca826aaabbab850
Fernando <fdomunozmartin@gmail.com>
2019-07-31 19:45:48 +0200

    added a column to carmen_data_utf.8 with the hashtags generated by the tf-idf. They don't seem acurate enough so we will have to think why and how to fix it.

--- create_keyword.R
+++ create_keyword.R
@@ -9,7 +9,7 @@ texts <- unnest_tokens(carmen_data_utf.8, word, work_decription)
 
 texts.tf.idf <- count(texts, work_ID, word, artist_name, work_title, sort = TRUE)
 
-texts.count <- texts.tf.idf %>% 
+ejemplo <- texts.tf.idf %>% 
   group_by(work_ID) %>% 
   summarise(total = sum(n))
 texts.tf.idf <- left_join(texts.tf.idf, texts.count)
@@ -23,3 +23,16 @@ texts.tf.idf <- texts.tf.idf %>%
   bind_tf_idf(word, work_ID, n)
 texts.tf.idf <- texts.tf.idf %>%
   arrange(desc(tf_idf))
+
+works.ID <- carmen_data_utf.8$work_ID
+texts.tf.idf.list <- list()
+for (i in 1:254) {
+  positions <- which(works.ID[i] == texts.tf.idf$work_ID)
+  texts.tf.idf.list[[i]] <- texts.tf.idf[positions,]
+}
+
+for (i in 1:length(texts.tf.idf.list)) {
+  carmen_data_utf.8$hashtags[i] <- c(texts.tf.idf.list[[i]][1:2,4])
+}
+texts.tf.idf$hashtags
+
