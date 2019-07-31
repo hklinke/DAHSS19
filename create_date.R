73692937fe799b7f7f19a93dfd219b1ffaaa8fa5
ciberciv <vcasasm@hotmail.es>
2019-07-31 14:07:46 +0200

    Add year of creation as creation_date to the dataset

--- create_date.R
+++ create_date.R
@@ -1,2 +1,15 @@
 #create_date.R
 message("create_date.R")
+
+parseDate = function(dateString) {
+  year = substr(dateString, nchar(dateString)-3, nchar(dateString))
+  
+  if (year == "s.f.")
+    return(NULL)
+  
+  return(as.numeric(year))
+}
+
+dates = carmen_data_utf.8$work_creaded
+
+carmen_data_utf.8$creation_date = Map(parseDate, dates)
