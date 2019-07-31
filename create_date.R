44d423d4bc10ec1bc3d72e540da1c1adf267bd40
ciberciv <vcasasm@hotmail.es>
2019-07-31 16:25:11 +0200

    Fix added column

--- create_date.R
+++ create_date.R
@@ -3,13 +3,14 @@ message("create_date.R")
 
 parseDate = function(dateString) {
   year = substr(dateString, nchar(dateString)-3, nchar(dateString))
+  year = as.numeric(year)
   
-  if (year == "s.f.")
+  if (is.na(year))
     return(NULL)
   
-  return(as.numeric(year))
+  return)
 }
 
 dates = carmen_data_utf.8$work_creaded
 
-carmen_data_utf.8$creation_date = Map(parseDate, dates)
+carmen_data_utf.8$finished_date = Map(parseDate, dates)
