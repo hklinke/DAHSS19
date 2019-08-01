710f4489737b1e76bc9ea9b27752b4409406afc8
Harald Klinke <h.klinke@lmu.de>
2019-08-01 16:52:15 +0200

    A great question added!

--- create_string.R
+++ create_string.R
@@ -1,16 +1,19 @@
 #create_string.R
 
-generateTweetText = function(artist, title, question, word, link) {
-  hashtag = paste("#", word, "")
-  question_and_answer = paste(question, hashtag, " ")
+generateTweetText = function(artist, title, question, word1, word2, link) {
+  hashtag1 = paste("#", word1, "")
+  hashtag2 = paste("#", word2, "")
+  question_and_answer = paste(question, hashtag1, hashtag2, " ")
   
   tweet_text = paste(artist, title, thyssen_handle, question_and_answer, link, sep = "\n")
 }
 
 artists = carmen_data_utf.8$artist.list
 titles = carmen_data_utf.8$work_title
-questions = carmen_data_utf.8$questions #This and word are considering them added to the data
-words = carmen_data_utf.8$word
+questions = "Guess the year!"   
+
+# check against stopwords and na
+words = carmen_data_utf.8$keyword_1  #carmen_data_utf.8$word
 links = carmen_data_utf.8$work_link.href
 
 carmen_data_utf.8$tweet_body = generateTweetText(artists, titles, questions, words, links)
