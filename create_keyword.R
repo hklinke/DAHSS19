86535fb9fce7f09ef299afc101310fd7733e27b7
Fernando <fdomunozmartin@gmail.com>
2019-08-01 14:53:29 +0200

    Keywords and gitignore updated

--- create_keyword.R
+++ create_keyword.R
@@ -1,38 +1,46 @@
 #create_keyword.R
 
-install.packages("tidytext")
-install.packages("dplyr")
-library(tidytext)
-library(dplyr)
+install.packages("udpipe")
+library(udpipe)
 
-texts <- unnest_tokens(carmen_data_utf.8, word, work_decription)
+ud_model_english=udpipe_download_model(language = "english") #Do this once only
+ud_model_spanish=udpipe_download_model(language = "spanish") #Do this once only
 
-texts.tf.idf <- count(texts, work_ID, word, artist_name, work_title, sort = TRUE)
-
-ejemplo <- texts.tf.idf %>% 
-  group_by(work_ID) %>% 
-  summarise(total = sum(n))
-texts.tf.idf <- left_join(texts.tf.idf, texts.count)
-# I could get the same result by writing a function that applies nchar() to every element in 
-# carmen_utf.8$work_decription and stores the result in the correspondent work_ID inside
-# texts.tf.idf, but this way is simplier.
+ud_model_english=udpipe_load_model(ud_model_english$file_model)
+ud_model_spanish=udpipe_load_model(ud_model_spanish$file_model)
 
 
-# tf-idf
-texts.tf.idf <- texts.tf.idf %>%
-  bind_tf_idf(word, work_ID, n)
-texts.tf.idf <- texts.tf.idf %>%
-  arrange(desc(tf_idf))
-
-works.ID <- carmen_data_utf.8$work_ID
-texts.tf.idf.list <- list()
 for (i in 1:254) {
-  positions <- which(works.ID[i] == texts.tf.idf$work_ID)
-  texts.tf.idf.list[[i]] <- texts.tf.idf[positions,]
+  
+  if (carmen_data_utf.8$work_ID == "CTB.2015.242"){
+    x=udpipe_annotate(ud_model_spanish, x=carmen_data_utf.8$work_decription[i])  #MUST be UTF8!
+    x=as.data.frame(x)
+    
+    stats=keywords_rake(x=x, term="lemma", group= c("doc_id")
+                        , relevant=x$upos %in% c("NOUN","ADJ"), ngram_max = 1)
+    
+    stats=subset(stats, stats$ngram==1)
+    
+    stats=stats[order(-stats$rake, -stats$freq),]
+    carmen_data_utf.8$keyword_1[i]=stats$keyword[1]
+    carmen_data_utf.8$keyword_2[i]=stats$keyword[2]
+    
+    message(i/254*100)   
+  } else {
+  
+  x=udpipe_annotate(ud_model_english, x=carmen_data_utf.8$work_decription[i])  #MUST be UTF8!
+  x=as.data.frame(x)
+  
+  stats=keywords_rake(x=x, term="lemma", group= c("doc_id")
+                      , relevant=x$upos %in% c("NOUN","ADJ"), ngram_max = 1)
+  
+  stats=subset(stats, stats$ngram==1)
+  
+  stats=stats[order(-stats$rake, -stats$freq),]
+  carmen_data_utf.8$keyword_1[i]=stats$keyword[1]
+  carmen_data_utf.8$keyword_2[i]=stats$keyword[2]
+  
+  message(i/254*100)
+  }
 }
-
-for (i in 1:length(texts.tf.idf.list)) {
-  carmen_data_utf.8$hashtags[i] <- c(texts.tf.idf.list[[i]][1:2,4])
-}
-texts.tf.idf$hashtags
 
