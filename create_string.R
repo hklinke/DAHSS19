cfb672ce2d94d2e5372681d0e0c14a5a0daee872
ciberciv <vcasasm@hotmail.es>
2019-08-01 12:09:36 +0200

    Add create_string functionality

--- create_string.R
+++ create_string.R
@@ -1,1 +1,16 @@
 #create_string.R
+
+generateTweetText = function(artist, title, question, word) {
+  hashtag = paste("#", word, "")
+  question = paste(question, hashtag, " ")
+  tweet_text = paste(artist, title, "@Thyssenmalaga", question, sep = "\n")
+}
+
+artists = carmen_data_utf.8$artist.list
+titles = carmen_data_utf.8$work_title
+questions = carmen_data_utf.8$questions #This and word are considering them added to the data
+words = carmen_data_utf.8$word
+
+carmen_data_utf.8$tweet_body = generateTweetText(artists, titles, questions, hashtags)
+
+carmen_data_utf.8$tweet_length = nchar(carmen_data_utf.8$tweet_body)
